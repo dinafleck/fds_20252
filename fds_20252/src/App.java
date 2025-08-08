82ccd3199d5bed6b929c561302c5fbd2837d6da7
Dinara <111661629+dinafleck@users.noreply.github.com>
2025-08-08 00:38:01 +0000

    alteracao completaOK

--- fds_20252/src/App.java
+++ fds_20252/src/App.java
@@ -1,6 +1,6 @@
 public class App {
  public static void main(String[] args) {
- System.out.println("\nHello world!");
+ System.out.println("\nHello new world!");
  String version = System.getProperty("java.version");
  System.out.println("Running Java Version: "+version+"\n");
  String name = System.getProperty("user.name");
